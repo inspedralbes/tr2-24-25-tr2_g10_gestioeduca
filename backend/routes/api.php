c8adbd883d0ab2556f2a9eae576c687ac1a4bb0c
adriapedralbes <a23adrestmar@inspedralbes.cat>
2024-12-03 18:07:07 +0100

    Configuracio per els migrate definida i automatitzacions per php artisan migrate amb makefile. Creat el model+controlador+table de Professors. Docker-compose modificat correctament per la persistencia de la bbdd

---
+++ backend/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfessorController;
+
+Route::get('/professors', [ProfessorController::class, 'index']);
+Route::get('/professors/{id}', [ProfessorController::class, 'show']);
+Route::post('/professors', [ProfessorController::class, 'store']);
