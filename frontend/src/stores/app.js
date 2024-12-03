cf23736dd5e9a03e786a18bc5640ed033b962b55
AraceliPac <a23arapacmun@inspedralbes.cat>
2024-12-03 15:57:10 +0100

    add global variables pin

--- frontend/src/stores/app.js
+++ frontend/src/stores/app.js
@@ -21,8 +21,45 @@ export const useAppStore = defineStore('app', () => {
     groupAssignments: [],
   });
 
-  const groupsInfo = ref([]);
+  const skillList = ref([
+    "Aprenentatge i utilització de coneixements",
+    "Autoconfiança",
+    "Autocontrol",
+    "Compromís amb l'organització",
+    "Comunicació",
+    "Creativitat",
+    "Direcció de persones",
+    "Empatia",
+    "Flexibilitat i gestió del canvi",
+    "Iniciativa",
+    "Lideratge",
+    "Negociació",
+    "Networking",
+    "Orientació a l'assoliment",
+    "Orientació al client",
+    "Orientació estratègica",
+    "Pensament analític",
+    "Pensament conceptual",
+    "Planificació i organització",
+    "Preocupació per l'ordre i la qualitat",
+    "Treball en equip i cooperació",
+  ]);
 
+  const groupsInfo = ref([]);
+  const roles = ref([
+    "M'agrada",
+    "No m'agrada",
+    "Empenta",
+    "Insulta",
+    "Exclou",
+    "Rumor",
+    "Ajuda",
+    "Anima",
+    "L'empenten",
+    "L'insulten",
+    "L'exclouen",
+    "Amics",
+  ]);
   // Actions
   const getCurrentUser = () => currentUser.value;
   const getStudentsList = () => studentsList.value;
@@ -30,7 +67,8 @@ export const useAppStore = defineStore('app', () => {
   const getStudentsAtRiskOfExclusion = () => studentsAtRiskOfExclusion.value;
   const getStudentProfile = () => studentProfile.value;
   const getGroupsInfo = () => groupsInfo.value;
-
+  const getSkillList = () => skillList.value;
+  const getRoles = () => roles.value;
   // Setters
   const setCurrentUser = (user) => {
     currentUser.value = user;
@@ -63,10 +101,15 @@ export const useAppStore = defineStore('app', () => {
     };
   }
 
+  const setSkillList = (skills) => {
+    skillList.value = skills;
+  }
   const setGroupsInfo = (groups) => {
     groupsInfo.value = groups;
   }
-
+  const setRoles = (roles) => {
+    roles.value = roles;
+  }
   return {
     currentUser: computed(getCurrentUser),
     studentsList: computed(getStudentsList),
@@ -80,5 +123,11 @@ export const useAppStore = defineStore('app', () => {
     setStudentsAtRiskOfExclusion,
     setStudentProfile,
     setGroupsInfo,
+    skillList,
+    setSkillList,
+    getSkillList,
+    roles,
+    getRoles,
+    setRoles,
   };
 });
