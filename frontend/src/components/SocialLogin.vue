b1609d11f5d587fc36db6848b82488033e595502
adriapedralbes <a23adrestmar@inspedralbes.cat>
2024-12-04 18:59:55 +0100

    Neteja de fitxers Vue creats per default y primera versio del login amb google

---
+++ frontend/src/components/SocialLogin.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { googleAuthCodeLogin } from "vue3-google-login";
+
+const gestioGoogleLogin = async () => {
+  try {
+    const response = await googleAuthCodeLogin();
+    console.log('Login amb Google correctament!', response);
+    //Enviar token al backend
+  } catch (error) {
+    console.error('Error al iniciar sessio amb Google:', error);
+  }
+};
+</script>
+
+<template>
+  <div class="social-login">
+    <button
+      class="social-button"
+      @click="gestioGoogleLogin"
+      aria-label="Entra amb Google"
+    >
+      <img 
+        src="/icons/google.svg" 
+        alt="Google icon"
+      >
+      <span>Google / @inspedralbes.cat</span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
