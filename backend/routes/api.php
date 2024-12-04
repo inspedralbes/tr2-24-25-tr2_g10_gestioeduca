eddb79d0e055da540ee419d977acf5d92d0fdf63
adriapedralbes <a23adrestmar@inspedralbes.cat>
2024-12-04 11:58:47 +0100

    creacio de model, controller y api d'alumnes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfessorController;
+use App\Http\Controllers\AlumnoController;
 
 Route::get('/professors', [ProfessorController::class, 'index']);
 Route::get('/professors/{id}', [ProfessorController::class, 'show']);
 Route::post('/professors', [ProfessorController::class, 'store']);
+
+Route::get('/alumnos', [AlumnoController::class, 'index']); // Obtener todos los alumnos
+Route::post('/alumnos', [AlumnoController::class, 'store']); // Crear un nuevo alumno
