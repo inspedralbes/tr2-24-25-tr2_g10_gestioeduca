23216a3ec97f08299fd991dac399003caf2b9224
adriapedralbes <a23adrestmar@inspedralbes.cat>
2024-12-05 09:24:04 +0100

    output de la instalacion de tailwind

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -46,7 +46,7 @@ const gestioSubmit = async (e) => {
   <div class="login-container">
     <div class="login-content">
       <div class="login-header">
-        <h1>Benvingut!</h1>
+        <h1 class="bg-red-500">Benvingut!</h1>
         <p>Gestió d'informació académica</p>
       </div>
 
@@ -87,5 +87,5 @@ const gestioSubmit = async (e) => {
 </template>
 
 <style scoped>
-
+  @import '@/assets/output.css';
 </style>
