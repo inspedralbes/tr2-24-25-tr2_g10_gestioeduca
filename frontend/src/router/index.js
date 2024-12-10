ae616c1eb068b39a28361787013e51bab543de29
AraceliPac <a23arapacmun@inspedralbes.cat>
2024-12-10 08:52:54 +0100

    add modify routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import LandingComponent from '@/components/Landing/LandingComponent.vue'
 import LoginView from '../views/LoginView.vue'
-import GroupList from '../views/Groups/GroupList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,36 +16,50 @@ const router = createRouter({
       name: 'login',
       component: LoginView,
     },
+    // {
+    //   path: '/login',
+    //   name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    //   component: () => import('../components/LoginComponent.vue'),
+    // },
     {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('../views/DashboardView.vue'),
-    },
-    {
-      path: '/studentsList',
-      name: 'alumnos',
-      component: () => import('../components/StudentsListComponent.vue'),
-    },
-    {
-      path: '/grupos',
-      name: 'GroupList',
-      component: GroupList
-    },
-    {
-      path: '/studentProfile',
+      path: '/studentProfile/:id',
       name: 'studentProfile',
       component: () => import('../components/StudentProfile/StudentProfileComponent.vue'),
     },
 
     {
-      path: '/studentTeacher',
-      name: 'studentTeacher',
+      path: '/teacherProfile/:id',
+      name: 'teacherProfile',
       component: () => import('../components/TeacherProfile/TeacherProfileComponent.vue'),
+    },
+
+    {
+     path: '/groupsProfile/:id',
+     name: 'groupsProfile',
+    component: () => import('../components/GroupsProfile/GroupProfileComponent.vue'),
+    },
+    {
+      path: '/studentsList',
+      name: 'studentsList',
+      component: () => import('../components/modules/user/StudentsListComponent.vue'),
     },
     {
       path: '/teachersList',
       name: 'teachersList',
-      component: () => import('../components/TeachersListComponent.vue'),
+      component: () => import('../components/modules/user/TeachersListComponent.vue'),
+    },
+    {
+      path: '/groupsList',
+      name: 'groupsList',
+      component: () => import('../components/modules/user/GroupsListComponent.vue'),
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../components/Dashboard/DashboardComponent.vue'),
     },
     {
       path: '/tancar-sessio', // Nueva ruta para cerrar sesión
